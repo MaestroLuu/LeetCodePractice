c7c2783bbff28d6d635c6e139c2a8df58c7d11c5
David Luu <dluu.audeo@gmail.com>
2023-02-18 21:24:32 -0800

    1480 Solved

---
+++ 1480. Running Sum of 1d Array/solution.js
@@ -0,0 +1,17 @@
+var runningSum = function(nums) {
+    let solArr = [];
+    let count = 0;
+    for (i = 0; i < nums.length; i++) {
+        count += nums[i];    
+        solArr.push(count);
+    }
+    return solArr;
+};
+
+let test1 = [1,2,3,4]
+let test2 = [1,1,1,1,1]
+let test3 = [3,1,2,10,1]
+
+console.log(runningSum(test1));
+console.log(runningSum(test2));
+console.log(runningSum(test3));
